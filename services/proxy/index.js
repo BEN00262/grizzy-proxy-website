c1f07f355832d19d1a8abca3e6ba45185b0779a9
john kerama <johnnesta2018@gmail.com>
2023-06-04 11:12:32 +0300

    removing ssl for now

--- services/proxy/index.js
+++ services/proxy/index.js
@@ -1,15 +1,17 @@
 const proxy = require('redbird')({
     port: process.env.PROXY_PORT || 3001,
-    
-    letsencrypt: {
-        path: __dirname + '/certs',
-        port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
-    },
 
-    ssl: {
-        http2: true,
-        port: 443
-    }
+    // letsencrypt: {
+    //     path: __dirname + '/certs',
+    //     port: 9999 // LetsEncrypt minimal web server port for handling challenges. Routed 80->9999, no need to open 9999 in firewall. Default 3000 if not defined.
+    // },
+
+    // ssl: {
+    //     http2: true,
+    //     port: 443
+    // },
+
+    cluster: 4
 });
 
 // const docker = require('redbird').docker;
