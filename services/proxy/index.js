aeb3eab25cf9a99534830a318c07f86251b0d833
john kerama <johnnesta2018@gmail.com>
2023-06-04 11:04:11 +0300

    added ssl port to redbird

--- services/proxy/index.js
+++ services/proxy/index.js
@@ -1,5 +1,9 @@
 const proxy = require('redbird')({
-    port: process.env.PROXY_PORT || 3001
+    port: process.env.PROXY_PORT || 3001,
+    ssl: {
+        http2: true,
+        port: 443
+    }
 });
 
 // const docker = require('redbird').docker;
