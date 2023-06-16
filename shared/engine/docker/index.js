2e17f2dc2a943801332e062c57ec31c58c8c6713
john kerama <johnnesta2018@gmail.com>
2023-06-16 12:20:37 +0300

    working on switching to using swarm - weekend

--- shared/engine/docker/index.js
+++ shared/engine/docker/index.js
@@ -1,71 +1,88 @@
 const Docker = require('dockerode');
-const portfinder = require('portfinder');
 const tar = require('tar-fs');
-const { nanoid } = require('nanoid');
 const { snakeCase } = require('snake-case');
 const { GrizzyDeployException, GrizzyInternalDeploymentException } = require('../../utils');
-
-portfinder.setBasePort(3001);
 
 function mbs_to_bytes(mb) {
     return mb * (1024 ** 2)
 }
 
+// SWITCH TO USING DOCKER SWARM -> KUBERNETES WILL COME LATER
 class SimpleHosterDocker {
     constructor() {
       this.docker = new Docker();
     }
 
+    /**
+     * @description responsible for creating a service and then starting the service from a service spec
+     * @param {string} app_name 
+     * @param {string[]} secrets_manager 
+     */
+    async createDeploymentService(
+        app_name, secrets_manager, replicas = 2
+    ) {
+        await this.docker.createService({
+            Name: app_name,
+            Networks: [{ Target: 'traefik' }],
+            Mode: {
+                Replicated: {
+                    Replicas: replicas // start with two replicas on launch
+                }
+            },
+
+            UpdateConfig: {
+                Parallelism: 1, // one at a time
+                Delay: 10 * 1000000000, // in nanoseconds
+                FailureAction: 'rollback', // other options -> continue, pause
+                Monitor: 15 * 1000000000,
+                MaxFailureRatio: 0.15,
+                Order: 'stop-first'
+            },
+
+            RollbackConfig: {
+                Parallelism: 1, // Number of tasks to rollback simultaneously
+                Delay: 5 * 1000000000, // Delay in nanoseconds before rolling back the next task
+                FailureAction: 'pause', // Action to take if a rollback fails (pause, continue)
+                Monitor: 10 * 1000000000, // Interval in nanoseconds to monitor task rollbacks
+            },
+
+            TaskTemplate: {
+                ContainerSpec: {
+                    Image: `${snakeCase(app_name)}:latest`, // the string interpolation is not really required but meeh
+                    Env: secrets_manager,
+                    Labels: {
+                        'traefik.enable': 'true',
+                        [`traefik.http.routers.${app_name}.rule`]: `Host(\`${app_name}.grizzy-deploy.com\`)`,
+                        [`traefik.http.services.${app_name}.loadbalancer.server.port`]: '3000'
+                    },
+
+                    // remove all privileges
+                    Privileges: {
+                        CredentialSpec: null
+                    },
+
+                    // limit a given container to only 512 of RAM at a go
+                    Resources: {
+                        Limits: {
+                          MemoryBytes: mbs_to_bytes(512),
+                        },
+                    },
+                }
+            }
+        });
+    }
+    
     // missing functionalities
     // snapshoting and storing the snapshot in s3 | replay the snapshot later
     // TODO: implement this later on
 
     // returns the port the container is running in
     async createContainerAndStart(app_name, secrets_manager, not_sure_exists_locally = false) {
-        // we can check if the image does exist locally if not pull it from the private repo
         const _app_name = snakeCase(app_name);
 
-        if (not_sure_exists_locally) {
-            // check if the image exists locally
-            const local_image = (await this.docker.getImage(_app_name).inspect())?.Id;
-
-            if (!local_image) {
-                const pull_stream = await this.docker.pull(`${process.env.DOCKERHUB_USERNAME}/${_app_name}:latest`, {
-                    authconfig: {
-                        username: process.env.DOCKERHUB_USERNAME,
-                        password: process.env.DOCKERHUB_PASSWORD,
-                    }
-                });
-
-                await new Promise((resolve, reject) => {
-                    this.docker.modem.followProgress(pull_stream, (err, res) => {
-                        if (err) {
-                            reject(err);
-                        }
-    
-                        resolve(res);
-                    });
-                });
-            }
-        }
-        
-        // const generated_port = await portfinder.getPortPromise();
-
-        // start using traefik to automatically register containers with domains
-
         const container = await this.docker.createContainer({
-            Image: _app_name, // image to spin up
-
-            // HANDLED BY TRAEFIK
-            // ExposedPorts: {
-            //     "3000/tcp": {}
-            // },
-
-            // we can also pass env keys pretty easily
-            // find a way to store env keys securely and load them to the container
-            // resolve the project secrets somehow
+            Image: _app_name,
             Env: secrets_manager,
-            // fix the app name --> just pass it the way it is only 
             Labels: {
                 'traefik.enable': 'true',
                 [`traefik.http.routers.${app_name}.rule`]: `Host(\`${app_name}.grizzy-deploy.com\`)`,
@@ -76,8 +93,7 @@ class SimpleHosterDocker {
             RestartPolicy: {
                 Name: 'always'
             },
-
-            // bind the container to the traefik network and let it be
+             
             NetworkingConfig: {
                 EndpointsConfig: {
                   traefik: {
@@ -87,29 +103,13 @@ class SimpleHosterDocker {
             },
 
             HostConfig: {
-                // use restart always --> check why it sucks
-
-                // AutoRemove: true,
                 Privileged: false,
                 CpuCount: 1,
-
-                // Isolation: "process",
                 Memory: mbs_to_bytes(512),
-
-                // HANDLED BY TRAEFIK -- this was redudant
-                // PortBindings: {
-                //     "3000/tcp": [{
-                //         "HostIP":"0.0.0.0",
-                //         "HostPort": `${generated_port}`
-                //     }]
-                // },
             }
         });
 
-        // start the container
         await container.start();
-
-        // return generated_port;
     }
 
     // we want to check if there are any containers running for a given image if not recreate and then return the port of the container
@@ -178,8 +178,8 @@ class SimpleHosterDocker {
         4. Build the image
      */
     async createImage(
-        app_name /* nanoid */, application_temp_directory, 
-        secrets_manager, logs_handler, run_immediately = true, instances = 1
+        app_name, application_temp_directory, secrets_manager, 
+        run_immediately = true, replicas = 2
     ) {
         try {
             let _app_name = snakeCase(app_name)
@@ -207,35 +207,21 @@ class SimpleHosterDocker {
                 });
             });
 
-            // get the image sha and return it
-            const image = this.docker.getImage(_app_name);
-
-            if (!image) {
-                throw new GrizzyDeployException("Failed to get the deployment id");
-            }
-
-            const image_version_id = (await image.inspect())?.Id;
-
-            // get the logs
-            let parsed_logs = logs?.map(({ stream }) => stream)
-                ?.filter(u => u || u !== '\n')?.join("\n");
-    
-            // check if we have to run immediately
             if (run_immediately) {
-                // how many instances to spin -- use that
-                const selfThis = this;
-
-                // switch to docker swarm to handle this madness
-                (await Promise.allSettled(
-                    // create the instances in parallel
-                    (new Array(instances).fill(1)).map(
-                        async _ => selfThis.createContainerAndStart(app_name, secrets_manager)
-                    )
-                )).map(({ value }) => value).filter(port => port /* a valid port */);
+                await this.createDeploymentService(
+                    app_name, secrets_manager,
+                    replicas // replicas
+                );
+            } else {
+                // scheduled deployments happen here
+                // send the jobs to @hokify/Agenda
             }
 
             // for scheduled deploys
-            return { image_version_id, logs: parsed_logs };
+            return { 
+                image_version_id: (await (this.docker.getImage(_app_name)).inspect())?.Id, 
+                logs: logs?.map(({ stream }) => stream)?.filter(u => u || u !== '\n')?.join("\n") 
+            };
         } catch (error) {
             // TODO: check the error if its a rethrowable as a GrizzyInternal one or just a regular error
             console.log(error);
